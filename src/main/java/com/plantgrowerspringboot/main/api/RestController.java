3e2a52ce352f6923193357b94573d9d5bed752c3
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-12-11 21:19:04 +0100

    crossOrigin for public endpoint

--- src/main/java/com/plantgrowerspringboot/main/api/RestController.java
+++ src/main/java/com/plantgrowerspringboot/main/api/RestController.java
@@ -13,10 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -73,6 +70,7 @@ public class RestController implements PlantGrowApi, PublicStatsApi {
     }
 
     @Override
+    @CrossOrigin()
     public ResponseEntity<Resource> getStatsImageName(String name, String contentType) {
         return publicStatsRequestHandler.getStatsImageByName(name, contentType);
     }
